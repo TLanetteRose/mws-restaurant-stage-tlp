e718ff270ef07b12e14239593662ce9ce16358af
T. Lanette <TLPollard4516@gmail.com>
2019-08-03 19:20:31 -0400

    Updated Project Files

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibGFudHJpc3BvbGxhIiwiYSI6ImNqeXZ2dGRpbjBuajAzY2wyNXkwZ2dkeWYifQ.m8WNh845drELhBtJhgjy5g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
---
+++ js/swregister.js
@@ -0,0 +1,10 @@
+//Service Worker 
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(function() {
+		console.log('Registration worked!');
+	})
+	.catch(function() {
+		console.log('Registration failed!');
+	});
+}
